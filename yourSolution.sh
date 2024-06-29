f69576a3a7e40400064ce4d0238a5b8e24a3d76d
tals <tal.silbiger@harmonicinc.com>
2024-06-29 13:22:31 +0300

    MySolutionProject1_fix15

--- yourSolution.sh
+++ yourSolution.sh
@@ -4,8 +4,10 @@ cd src/
 rm -r maliciousFiles
 rm important.link
 mkdir secretDir
+chmod o+rw secretDir/
 touch secretDir/.secret
 chmod o-rw secretDir/.secret
 chmod g-rw secretDir/.secret
 /bin/bash generateSecret.sh
 cat secretDir/.secret
+
